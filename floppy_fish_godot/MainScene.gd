dfc842f0d8563a00e1e277f0fb76bf40d19440d5
jitspoe <jitspoe@yahoo.com>
2021-10-26 10:08:47 -0400

    Added subtitle system.

--- floppy_fish_godot/MainScene.gd
+++ floppy_fish_godot/MainScene.gd
@@ -1,13 +1,36 @@
 extends Spatial
 
+const MIN_TIME_BETWEEN_VO := 1.0
 onready var MainCamera : Camera = $Camera
 onready var Fish : Spatial = $Fish
+onready var SubtitleLabel : Label = $HUD/Subtitle
+onready var VOPlayer : AudioStreamPlayer = $VOPlayer
 var GameTime := 0.0
 var Completed := false
+var TimeSinceVOPlayed := {}
+var VOQueue := []
+var TimeSinceLastVO := 99999999.9
 
+class VOQueueData:
+	var TimeQueued := 0.0
+	var TimeToSkip := 0.0
+	var VOLine : String
 
 func _ready():
 	$EndScreen.visible = false
+	SubtitleLabel.visible = false
+	OS.window_maximized = true
+
+
+func _process(delta : float):
+	TimeSinceLastVO += delta
+	if (VOPlayer.playing):
+		TimeSinceLastVO = 0.0
+	if (TimeSinceLastVO > 0.0):
+		if (TimeSinceLastVO > 1.0):
+			SubtitleLabel.visible = false
+		else:
+			SubtitleLabel.modulate.a = (1.0 - TimeSinceLastVO)
 
 
 func _physics_process(delta : float):
@@ -43,9 +66,24 @@ func _on_EndTrigger_body_entered(body):
 		$EndScreen/CenterContainer/GridContainer/Label.text = "Completed in %s" % TimeFormat(GameTime)
 		$SoundSplashEnd.global_transform.origin = body.global_transform.origin
 		$SoundSplashEnd.play()
+		QueueVO("buoy_did_it")
 		print("You win.  Time:", GameTime)
 		Completed = true
 
 
 func _on_RestartButton_pressed():
 	call_deferred("Restart")
+
+
+func PlayVO(VOLine : String):
+	SubtitleLabel.visible = true
+	SubtitleLabel.modulate.a = 1.0
+	SubtitleLabel.text = tr(VOLine)
+	VOPlayer.stream = load("res://VO/%s.ogg" % VOLine)
+	VOPlayer.play()
+
+
+func QueueVO(VOLine : String, TimeBeforeSkip := 3.0):
+	
+	PlayVO(VOLine)
+	pass
