52803ea1cc16f3e3b758bd82be900695c63f6a2e
jitspoe <jitspoe@yahoo.com>
2021-10-25 20:52:00 -0400

    Added fish mesh, recorded some sounds.

--- floppy_fish_godot/MainScene.gd
+++ floppy_fish_godot/MainScene.gd
@@ -4,5 +4,5 @@ onready var MainCamera : Camera = $Camera
 onready var Fish : Spatial = $Fish
 
 func _physics_process(_delta : float):
-	MainCamera.global_transform.origin.y = Fish.AverageLocation.y + 0.3
+	MainCamera.global_transform.origin.y = Fish.AverageLocation.y + 0.6
 	MainCamera.global_transform.origin.x = Fish.AverageLocation.x
